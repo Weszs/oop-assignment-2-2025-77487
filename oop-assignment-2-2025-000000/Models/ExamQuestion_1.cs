ff8e95f177dded756afcde642b84e116b74c62e6
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-12-08 21:46:07 +0000

    Added messy code for refactoring

--- oop-assignment-2-2025-000000/Models/ExamQuestion_1.cs
+++ oop-assignment-2-2025-000000/Models/ExamQuestion_1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace oop_assignment_2_2025_000000.Models
@@ -11,6 +12,7 @@ namespace oop_assignment_2_2025_000000.Models
             Console.WriteLine("Exam Question 1 executed.");
             SampleQuestionAMethod();
             SampleQuestionBMethod();
+            Console.WriteLine(GetDiscountPercentage("Bronze"));
         }
         public static void SampleQuestionAMethod()
         {
@@ -23,5 +25,59 @@ namespace oop_assignment_2_2025_000000.Models
             // Sample method implementation
             Console.WriteLine("SampleMethod");
         }
+        public static int GetDiscountPercentage(string level)
+        {
+            if (level == "Bronze")
+            {
+                return 1;
+            }
+            else
+            {
+                if (level == "Silver")
+                {
+                    return 5;
+                }
+                else
+                {
+                    if (level == "Gold")
+                    {
+                        return 10;
+                    }
+                    else
+                    {
+                        if (level == "Platinum")
+                        {
+                            return 15;
+                        }
+                        else
+                        {
+                            if (level == "Diamond")
+                            {
+                                return 20;
+                            }
+                            else
+                            {
+                                if (level == "Elite")
+                                {
+                                    return 25;
+                                }
+                                else
+                                {
+                                    if (level == "VIP")
+                                    {
+                                        return 30;
+                                    }
+                                    else
+                                    {
+                                        return 0; // Unknown level
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
